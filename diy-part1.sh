4f14b691d1c2d372699c0af1f7ecfddaef15c017
xiaochen66666 <150781778+xiaochen66666@users.noreply.github.com>
2026-08-07 15:59:52 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,77 +1,26 @@
-#!/bin/bash
+# Add a feed source
+echo '添加 whiskyrye 软件源'
+# 1. 先尝试移除可能存在的旧配置，避免重复
+sed -i '/whiskyrye\/ImmortalWrt-Packages/d' feeds.conf.default
+# 2. 添加新源，使用更明确的名称
+echo 'src-git whiskyrye https://github.com/whiskyrye/ImmortalWrt-Packages.git;main' >> feeds.conf.default
+echo '=========Add whiskyrye feed OK!========='
 
-# https://github.com/coolsnowwolf/lede.git
-
-# Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# echo '添加SSR-Plus软件源'
-# sed -i "/helloworld/d" "feeds.conf.default"
-# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-# echo '=========Add SSR-Plus source OK!========='
-
-echo '添加Passwall依赖feeds'
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
-echo '=========Add passwall feeds source OK!========='
-
-echo '添加Passwall软件源'
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
-echo '=========Add passwall source OK!========='
-
-echo '添加jerrykuku的argon-mod主题'
-rm -rf package/lean/luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
-echo '=========Add argon-mod OK!========='
-
-echo '添加AdguardHome'
-# rm -rf feeds/kiddin9/adguardhome
-# rm -rf feeds/kiddin9/luci-app-adguardhome
-# rm -rf package/feeds/kiddin9/luci-app-adguardhome
-# rm -rf package/feeds/kiddin9/adguardhome
-rm -rf package/lean/luci-app-adguardhome
-git clone https://github.com/kongfl888/luci-app-adguardhome package/lean/luci-app-adguardhome 
-echo '=========Add AdguardHome OK!========='
-
-echo '添加lwz322的K3屏幕插件'
-rm -rf package/lean/luci-app-k3screenctrl
-git clone https://github.com/JE668/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
+echo '添加 K3 屏幕插件 (Luci 界面)'
+rm -rf package/luci-app-k3screenctrl
+git clone https://github.com/JE668/luci-app-k3screenctrl.git package/luci-app-k3screenctrl
 echo '=========Add k3screen plug OK!========='
 
-echo '替换lwz322的K3屏幕驱动插件'
-rm -rf package/lean/k3screenctrl
-git clone https://github.com/JE668/k3screenctrl_build.git package/lean/k3screenctrl/
+echo '添加 K3 屏幕驱动插件'
+rm -rf package/k3screenctrl
+git clone https://github.com/JE668/k3screenctrl_build.git package/k3screenctrl/
 echo '=========Replace k3screen drive plug OK!========='
 
-
-
-# echo '添加Haiibo软件源'
-# sed -i '1i src-git haiibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
-# echo '=========Add Haiibo source OK!========='
-
-
-# echo '添加kenzok8 small-package'
-# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-# echo '=========Add kenzok8 source OK!========='
-
-# echo '添加kiddin9软件源'
-# echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
-# echo '=========Add kiddin9 source OK!========='
-
-
-echo '添加mproxy软件源'
-echo 'src-git mproxy https://github.com/chenzhen6666/luci-app-mproxy.git' >>feeds.conf.default
-echo '=========Add mproxy source OK!========='
-
-#1.'asus_dhd24' 2.'ac88u_20' 3.'69027'
+# 无线驱动版本选择 (1.'asus_dhd24' 2.'ac88u_20' 3.'69027')
 firmware='69027'
 echo '替换无线驱动'
-wget -nv https://github.com/JE668/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+wget -nv https://github.com/JE668/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/firmware/brcmfmac4366c0-firmware-k3/files/brcmfmac4366c-pcie.bin
 echo '=========Replace k3 wireless firmware OK!========='
-
-# 设置wifi功率
-sed -i '3a iwconfig wlan0 txpower 24' package/base-files/files/etc/rc.local
-sed -i '4a iwconfig wlan1 txpower 24' package/base-files/files/etc/rc.local
 
 #Compile K3 Only
 sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
